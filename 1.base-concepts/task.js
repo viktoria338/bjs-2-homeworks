3ece82a59f8c74d5527ffedad859567a5762b3b9
Виктория Дворникова <wika.daw@mail.ru>
2022-08-09 13:24:06 +0300

    first commit variables

--- 1.base-concepts/task.js
+++ 1.base-concepts/task.js
@@ -1,13 +1,54 @@
+"use strict";
+
 function solveEquation(a, b, c) {
-  let arr;
-  // код для задачи №1 писать здесь
+  let arr = [];
+  let oneRoot;
+  let twoRoot;
+  let d = b ** 2 - 4 * a * c;
+
+  if (d === 0) {
+    oneRoot = -b / (2 * a);
+    arr.push(oneRoot)
+  } else if (d > 0) {
+    oneRoot = (-b + Math.sqrt(d)) / (2 * a);
+    twoRoot = (-b - Math.sqrt(d)) / (2 * a)
+    arr.push(oneRoot, twoRoot)
+  }
+
   return arr; // array
 }
 
 function calculateTotalMortgage(percent, contribution, amount, date) {
-  let totalAmount;
+  if (typeof percent !== 'number') {
+    return `Параметр "Процентная ставка" содержит неправильное значение "${percent}"`
+  }
+  if (typeof contribution !== 'number') {
+    return `Параметр "Процентная ставка" содержит неправильное значение "${contribution}"`
+  }
+  if (typeof amount !== 'number') {
+    return `Параметр "Процентная ставка" содержит неправильное значение "${amount}"`
+  }
 
-  // код для задачи №2 писать здесь
+
+  let totalAmount;
+  let s = amount - contribution;
+
+  let nowDate = new Date();
+  let year1 = nowDate.getFullYear();
+  let year2 = date.getFullYear();
+  let month1 = nowDate.getMonth();
+  let month2 = date.getMonth();
+  if (month1 === 0) {
+    month1++;
+    month2++;
+  }
+  let n = (year2 - year1) * 12 + (month2 - month1);
+  let p = percent / 12 / 100;
+
+  let amountPerMonth = s * (p + (p / (((1 + p)**n) - 1)));
+
+  totalAmount=(amountPerMonth * n);
+  totalAmount=Number(totalAmount.toFixed(2));
 
   return totalAmount;
 }
