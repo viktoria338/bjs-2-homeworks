119f07d908546aa40eec1b490ecd69a3b0c0a78d
Виктория Дворникова <wika.daw@mail.ru>
2022-08-09 19:33:14 +0300

    домашняя работа 3

--- 3.arrays/task.js
+++ 3.arrays/task.js
@@ -1,15 +1,12 @@
+
+
 function compareArrays(arr1, arr2) {
-  let result;
-
-  // Ваш код
-
-  return result; // boolean
+  let result = arr1.length === arr2.length && arr1.every((item,index) => arr2.indexOf(item) == index);
+  return result
 }
 
 function advancedFilter(arr) {
-  let resultArr;
-
-  // Ваш код
+  let resultArr = arr.filter(item => item >= 0 && item % 3 === 0).map(item => item * 10)
 
   return resultArr; // array
 }
