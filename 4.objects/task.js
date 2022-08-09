9b1109e85f2349bece6bc55a4dc730d118b330d2
Виктория Дворникова <wika.daw@mail.ru>
2022-08-09 19:37:06 +0300

    домашняя работа 4

--- 4.objects/task.js
+++ 4.objects/task.js
@@ -1,10 +1,38 @@
-function Student(name, gender, age) {
-    // Ваш код
 
+function Student(name, gender, age) {
+  this.name = name
+  this.gender = gender
+  this.age = age
 }
 
 Student.prototype.setSubject = function (subjectName) {
-  //ваш код
+ this.subject = subjectName;
 }
 
-// ваш код для остальных методов
+Student.prototype.addMark = function (mark) {
+  if(this.marks === undefined) {
+    this.marks = [];
+    this.marks.push(mark)
+  } else {
+    this.marks.push(mark)
+  }
+}
+
+Student.prototype.addMarks = function (...marks) {
+  if(this.marks === undefined) {
+    this.marks = [];
+    this.marks.push(...marks)
+  } else {
+    this.marks.push(...marks)
+  }
+}
+
+Student.prototype.getAverage = function () {
+  return this.marks.reduce((sum, amount) => sum += amount) / this.marks.length;
+}
+
+Student.prototype.exclude = function (reason) {
+  delete this.subject
+  delete this.marks
+  this.excluded = reason;
+}
