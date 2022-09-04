134e5b31d14d9e6cd2485e9a35224759e365d3ed
Виктория Дворникова <wika.daw@mail.ru>
2022-09-04 12:06:34 +0300

    домашняя работа 8

--- 8.decorators/task.js
+++ 8.decorators/task.js
@@ -1,12 +1,61 @@
 function cachingDecoratorNew(func) {
-  // Ваш код
+  let cache = [];
+
+  function wrapper(...args) {
+    const hash = args.join(',');
+    let objectInCache = cache.find((item) => item.hash === hash);
+    if (objectInCache) {
+      console.log("Из кэша: " + objectInCache.value);
+      return "Из кэша: " + objectInCache.value;
+    }
+
+    let result = func(...args);
+    cache.push({ hash, value: result });
+    if (cache.length > 5) {
+      cache.shift();
+    }
+    console.log("Вычисляем: " + result);
+    return "Вычисляем: " + result;
+  }
+  return wrapper
 }
 
 
-function debounceDecoratorNew(func) {
-  // Ваш код
+function debounceDecoratorNew(func, ms) {
+  let timerId;
+
+  return function wrapper(...args) {
+    if (timerId == undefined) {
+      func.apply(this, args);
+    }
+
+
+    clearTimeout(timerId);
+
+    timerId = setTimeout(() => {
+      func.apply(this, args)
+    }, ms);
+  }
 }
+
+
+
 
 function debounceDecorator2(func) {
-  // Ваш код
+  let timerId;
+  wrapper.count = 0;
+  return function wrapper(...args) {
+    if (timerId == undefined) {
+      func.apply(this, args);
+      wrapper.count++;
+    }
+
+    clearTimeout(timerId);
+
+    timerId = setTimeout(() => {
+      func.apply(this, args)
+    }, ms);
+  }
+
+
 }
